18046ddd4bd4768e4375f3ee6b1694f17fbf773c
Trustin Lee <t@motd.kr>
2016-11-04 02:44:07 +0900

    Fix a bug where panic() does not replace tabs and carriage returns correctly

--- src/main/java/kr/motd/gleamstream/Panic.java
+++ src/main/java/kr/motd/gleamstream/Panic.java
@@ -1,29 +1,21 @@
 package kr.motd.gleamstream;
 
+import java.util.regex.Pattern;
+
 import org.lwjgl.util.tinyfd.TinyFileDialogs;
 
 import com.google.common.base.Throwables;
 
 public final class Panic {
 
+    private static final Pattern TAB_PATTERN = Pattern.compile("\t", Pattern.LITERAL);
+    private static final Pattern NEWLINE_PATTERN = Pattern.compile("\r?\n");
+
     public static RuntimeException panic(String message, Throwable cause) {
         MainWindow.INSTANCE.destroy();
 
-        message += '\n' + Throwables.getStackTraceAsString(cause);
-        final String[] lines = message.replace(System.lineSeparator(), "\n")
-                                      .replace("\t", "    ").split("\n");
-
-        final StringBuilder buf = new StringBuilder();
-        final int end = Math.min(lines.length, 10);
-        for (int i = 0;;) {
-            buf.append(lines[i]);
-            if (++i == end) {
-                break;
-            }
-            buf.append(System.lineSeparator());
-        }
-
-        TinyFileDialogs.tinyfd_messageBox("Error", message, "ok", "error", true);
+        TinyFileDialogs.tinyfd_messageBox("Error", toString(message, cause),
+                                          "ok", "error", true);
         System.exit(1);
 
         return new RuntimeException("panic!") {
@@ -42,6 +34,25 @@ public final class Panic {
 
     public static RuntimeException panic(String message) {
         return panic(message, new IllegalStateException("panic!"));
+    }
+
+    private static String toString(String message, Throwable cause) {
+        final String messageWithTrace =
+                TAB_PATTERN.matcher(message + '\n' + Throwables.getStackTraceAsString(cause))
+                           .replaceAll("    ");
+
+        final String[] lines = NEWLINE_PATTERN.split(messageWithTrace);
+        final StringBuilder buf = new StringBuilder();
+        final int end = Math.min(lines.length, 10);
+        for (int i = 0;;) {
+            buf.append(lines[i]);
+            if (++i == end) {
+                break;
+            }
+            buf.append(System.lineSeparator());
+        }
+
+        return buf.toString();
     }
 
     private Panic() {}
