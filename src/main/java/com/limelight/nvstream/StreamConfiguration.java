ddbed01a4ff36b20f03f7cc4633104afcab6fbaf
Trustin Lee <t@motd.kr>
2016-11-04 10:49:28 +0900

    Clean up StreamConfiguration

--- src/main/java/com/limelight/nvstream/StreamConfiguration.java
+++ src/main/java/com/limelight/nvstream/StreamConfiguration.java
@@ -28,55 +28,55 @@ public class StreamConfiguration {
     private boolean supportsHevc;
 
     public static class Builder {
-        private StreamConfiguration config = new StreamConfiguration();
+        private final StreamConfiguration config = new StreamConfiguration();
 
-        public StreamConfiguration.Builder setApp(NvApp app) {
+        public Builder setApp(NvApp app) {
             config.app = app;
             return this;
         }
 
-        public StreamConfiguration.Builder setRemote(boolean remote) {
+        public Builder setRemote(boolean remote) {
             config.remote = remote;
             return this;
         }
 
-        public StreamConfiguration.Builder setResolution(int width, int height) {
+        public Builder setResolution(int width, int height) {
             config.width = width;
             config.height = height;
             return this;
         }
 
-        public StreamConfiguration.Builder setRefreshRate(int refreshRate) {
+        public Builder setRefreshRate(int refreshRate) {
             config.refreshRate = refreshRate;
             return this;
         }
 
-        public StreamConfiguration.Builder setBitrate(int bitrate) {
+        public Builder setBitrate(int bitrate) {
             config.bitrate = bitrate;
             return this;
         }
 
-        public StreamConfiguration.Builder setEnableSops(boolean enable) {
+        public Builder setEnableSops(boolean enable) {
             config.sops = enable;
             return this;
         }
 
-        public StreamConfiguration.Builder enableAdaptiveResolution(boolean enable) {
+        public Builder enableAdaptiveResolution(boolean enable) {
             config.enableAdaptiveResolution = enable;
             return this;
         }
 
-        public StreamConfiguration.Builder enableLocalAudioPlayback(boolean enable) {
+        public Builder enableLocalAudioPlayback(boolean enable) {
             config.playLocalAudio = enable;
             return this;
         }
 
-        public StreamConfiguration.Builder setMaxPacketSize(int maxPacketSize) {
+        public Builder setMaxPacketSize(int maxPacketSize) {
             config.maxPacketSize = maxPacketSize;
             return this;
         }
 
-        public StreamConfiguration.Builder setAudioConfiguration(int audioConfig) {
+        public Builder setAudioConfiguration(int audioConfig) {
             if (audioConfig == AUDIO_CONFIGURATION_STEREO) {
                 config.audioChannelCount = CHANNEL_COUNT_STEREO;
                 config.audioChannelMask = CHANNEL_MASK_STEREO;
@@ -90,7 +90,7 @@ public class StreamConfiguration {
             return this;
         }
 
-        public StreamConfiguration.Builder setHevcSupported(boolean supportsHevc) {
+        public Builder setHevcSupported(boolean supportsHevc) {
             config.supportsHevc = supportsHevc;
             return this;
         }
@@ -102,17 +102,17 @@ public class StreamConfiguration {
 
     private StreamConfiguration() {
         // Set default attributes
-        this.app = new NvApp("Steam");
-        this.width = 1280;
-        this.height = 720;
-        this.refreshRate = 60;
-        this.bitrate = 10000;
-        this.maxPacketSize = 1024;
-        this.sops = true;
-        this.enableAdaptiveResolution = false;
-        this.audioChannelCount = CHANNEL_COUNT_STEREO;
-        this.audioChannelMask = CHANNEL_MASK_STEREO;
-        this.supportsHevc = false;
+        app = new NvApp("Steam");
+        width = 1280;
+        height = 720;
+        refreshRate = 60;
+        bitrate = 10000;
+        maxPacketSize = 1024;
+        sops = true;
+        enableAdaptiveResolution = false;
+        audioChannelCount = CHANNEL_COUNT_STEREO;
+        audioChannelMask = CHANNEL_MASK_STEREO;
+        supportsHevc = false;
     }
 
     public int getWidth() {
